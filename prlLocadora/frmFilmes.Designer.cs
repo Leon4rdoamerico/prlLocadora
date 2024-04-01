ab5825c5b3afdf61de97e31b472f83f29d6feb1f
Leonardo Americo <leonardoamerico1455@gmail.com>
2024-04-01 20:44:14 -0300

    Commit configuracao janela filmes

--- prlLocadora/frmFilmes.Designer.cs
+++ prlLocadora/frmFilmes.Designer.cs
@@ -36,20 +36,20 @@ namespace prlLocadora
             this.txtAno = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.cbbProdutora = new System.Windows.Forms.ComboBox();
-            this.cbbGenero = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.btnUltimo = new System.Windows.Forms.Button();
-            this.btnProximo = new System.Windows.Forms.Button();
-            this.btnAnterior = new System.Windows.Forms.Button();
-            this.btnPrimeiro = new System.Windows.Forms.Button();
             this.btnAlterar = new System.Windows.Forms.Button();
             this.btnExcluir = new System.Windows.Forms.Button();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnNovo = new System.Windows.Forms.Button();
+            this.btnUltimo = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbbGenero = new System.Windows.Forms.ComboBox();
+            this.btnProximo = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbbProdutora = new System.Windows.Forms.ComboBox();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnPrimeiro = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -133,106 +133,6 @@ namespace prlLocadora
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
-            // cbbProdutora
-            // 
-            this.cbbProdutora.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbbProdutora.FormattingEnabled = true;
-            this.cbbProdutora.Location = new System.Drawing.Point(119, 35);
-            this.cbbProdutora.Name = "cbbProdutora";
-            this.cbbProdutora.Size = new System.Drawing.Size(237, 33);
-            this.cbbProdutora.TabIndex = 6;
-            // 
-            // cbbGenero
-            // 
-            this.cbbGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbbGenero.FormattingEnabled = true;
-            this.cbbGenero.Location = new System.Drawing.Point(89, 32);
-            this.cbbGenero.Name = "cbbGenero";
-            this.cbbGenero.Size = new System.Drawing.Size(268, 33);
-            this.cbbGenero.TabIndex = 7;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(6, 35);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(77, 25);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Gênero";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(6, 34);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 25);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Produtora";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.groupBox3.Controls.Add(this.label4);
-            this.groupBox3.Controls.Add(this.cbbGenero);
-            this.groupBox3.Location = new System.Drawing.Point(443, 267);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(410, 96);
-            this.groupBox3.TabIndex = 11;
-            this.groupBox3.TabStop = false;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Controls.Add(this.cbbProdutora);
-            this.groupBox2.Location = new System.Drawing.Point(18, 267);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(410, 96);
-            this.groupBox2.TabIndex = 10;
-            this.groupBox2.TabStop = false;
-            // 
-            // btnUltimo
-            // 
-            this.btnUltimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUltimo.Location = new System.Drawing.Point(587, 399);
-            this.btnUltimo.Name = "btnUltimo";
-            this.btnUltimo.Size = new System.Drawing.Size(117, 42);
-            this.btnUltimo.TabIndex = 10;
-            this.btnUltimo.Text = ">>";
-            this.btnUltimo.UseVisualStyleBackColor = true;
-            // 
-            // btnProximo
-            // 
-            this.btnProximo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProximo.Location = new System.Drawing.Point(447, 399);
-            this.btnProximo.Name = "btnProximo";
-            this.btnProximo.Size = new System.Drawing.Size(117, 42);
-            this.btnProximo.TabIndex = 9;
-            this.btnProximo.Text = ">";
-            this.btnProximo.UseVisualStyleBackColor = true;
-            // 
-            // btnAnterior
-            // 
-            this.btnAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAnterior.Location = new System.Drawing.Point(311, 399);
-            this.btnAnterior.Name = "btnAnterior";
-            this.btnAnterior.Size = new System.Drawing.Size(117, 42);
-            this.btnAnterior.TabIndex = 8;
-            this.btnAnterior.Text = "<";
-            this.btnAnterior.UseVisualStyleBackColor = true;
-            // 
-            // btnPrimeiro
-            // 
-            this.btnPrimeiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnPrimeiro.Location = new System.Drawing.Point(175, 399);
-            this.btnPrimeiro.Name = "btnPrimeiro";
-            this.btnPrimeiro.Size = new System.Drawing.Size(117, 42);
-            this.btnPrimeiro.TabIndex = 7;
-            this.btnPrimeiro.Text = "<<";
-            this.btnPrimeiro.UseVisualStyleBackColor = true;
-            // 
             // btnAlterar
             // 
             this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -273,6 +173,116 @@ namespace prlLocadora
             this.btnNovo.Text = "Novo";
             this.btnNovo.UseVisualStyleBackColor = true;
             // 
+            // btnUltimo
+            // 
+            this.btnUltimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUltimo.Location = new System.Drawing.Point(587, 399);
+            this.btnUltimo.Name = "btnUltimo";
+            this.btnUltimo.Size = new System.Drawing.Size(117, 42);
+            this.btnUltimo.TabIndex = 10;
+            this.btnUltimo.Text = ">>";
+            this.btnUltimo.UseVisualStyleBackColor = true;
+            this.btnUltimo.Click += new System.EventHandler(this.btnUltimo_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.cbbGenero);
+            this.groupBox3.Location = new System.Drawing.Point(443, 267);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(410, 96);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gênero";
+            // 
+            // cbbGenero
+            // 
+            this.cbbGenero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbGenero.FormattingEnabled = true;
+            this.cbbGenero.Items.AddRange(new object[] {
+            "Suspense",
+            "Comédia",
+            "Terror",
+            "Drama",
+            "Romance"});
+            this.cbbGenero.Location = new System.Drawing.Point(89, 32);
+            this.cbbGenero.Name = "cbbGenero";
+            this.cbbGenero.Size = new System.Drawing.Size(268, 33);
+            this.cbbGenero.TabIndex = 7;
+            // 
+            // btnProximo
+            // 
+            this.btnProximo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProximo.Location = new System.Drawing.Point(447, 399);
+            this.btnProximo.Name = "btnProximo";
+            this.btnProximo.Size = new System.Drawing.Size(117, 42);
+            this.btnProximo.TabIndex = 9;
+            this.btnProximo.Text = ">";
+            this.btnProximo.UseVisualStyleBackColor = true;
+            this.btnProximo.Click += new System.EventHandler(this.btnProximo_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cbbProdutora);
+            this.groupBox2.Location = new System.Drawing.Point(18, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(410, 96);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Produtora";
+            // 
+            // cbbProdutora
+            // 
+            this.cbbProdutora.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbbProdutora.FormattingEnabled = true;
+            this.cbbProdutora.Location = new System.Drawing.Point(119, 35);
+            this.cbbProdutora.Name = "cbbProdutora";
+            this.cbbProdutora.Size = new System.Drawing.Size(237, 33);
+            this.cbbProdutora.TabIndex = 6;
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnterior.Location = new System.Drawing.Point(311, 399);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(117, 42);
+            this.btnAnterior.TabIndex = 8;
+            this.btnAnterior.Text = "<";
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            this.btnAnterior.Click += new System.EventHandler(this.btnAnterior_Click);
+            // 
+            // btnPrimeiro
+            // 
+            this.btnPrimeiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimeiro.Location = new System.Drawing.Point(175, 399);
+            this.btnPrimeiro.Name = "btnPrimeiro";
+            this.btnPrimeiro.Size = new System.Drawing.Size(117, 42);
+            this.btnPrimeiro.TabIndex = 7;
+            this.btnPrimeiro.Text = "<<";
+            this.btnPrimeiro.UseVisualStyleBackColor = true;
+            this.btnPrimeiro.Click += new System.EventHandler(this.btnPrimeiro_Click);
+            // 
             // frmFilmes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -283,6 +293,7 @@ namespace prlLocadora
             this.Name = "frmFilmes";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Filmes";
+            this.Load += new System.EventHandler(this.frmFilmes_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox3.ResumeLayout(false);
--- prlLocadora/frmFilmes.cs
+++ prlLocadora/frmFilmes.cs
@@ -7,14 +7,132 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace prlLocadora
 {
     public partial class frmFilmes : Form
     {
+        String connectionString = @"Server=DARNASSUS\MOTORHEAD;Database=db_230776;User Id=230776;Password=@166116";
+        int registroAtual = 0;
+        int totalregistros = 0;
+        bool novo;
+        DataTable dtFilmes = new DataTable();
+        DataTable dtProdutoras = new DataTable();
+
+        private void navegar()
+        {
+            carregaComboProdutoras();
+            txtCodFilme.Text = dtFilmes.Rows[registroAtual][0].ToString();
+            txtTituloFilme.Text = dtFilmes.Rows[registroAtual][1].ToString();
+            txtAno.Text = dtFilmes.Rows[registroAtual][2].ToString();
+            //cbbProdutora.Text = dtFilmes.Rows[registroAtual][3].ToString();
+            cbbGenero.Text = dtFilmes.Rows[registroAtual][4].ToString();
+        }
+
+        private void carregaComboProdutoras()
+        {
+            dtProdutoras = new DataTable();
+            string sql = "SELECT * FROM  tblProdutora WHERE codProd=" +
+                 dtFilmes.Rows[registroAtual][3].ToString();
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+            try
+            {
+                using (reader = cmd.ExecuteReader())
+                {
+                    dtProdutoras.Load(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            cbbProdutora.DataSource = dtProdutoras;
+            cbbProdutora.DisplayMember = "nomeProd";
+            cbbProdutora.ValueMember = "codProd";
+        }
+
         public frmFilmes()
         {
             InitializeComponent();
+        }
+
+        private void frmFilmes_Load(object sender, EventArgs e)
+        {
+            btnSalvar.Enabled = false;
+            txtCodFilme.Enabled = false;
+            txtTituloFilme.Enabled = false;
+            txtAno.Enabled = false;
+            cbbProdutora.Enabled = false;
+            cbbGenero.Enabled = false;
+            string sql = "SELECT * FROM tblFilme";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.CommandType = System.Data.CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+            try
+            {
+                using (reader = cmd.ExecuteReader())
+                {
+                    dtFilmes.Load(reader);
+                    totalregistros = dtFilmes.Rows.Count;
+                    registroAtual = 0;
+                    navegar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnProximo_Click(object sender, EventArgs e)
+        {
+            if(registroAtual < totalregistros - 1)
+            {
+                registroAtual++;
+                navegar();
+            }
+        }
+
+        private void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (registroAtual > 0)
+            {
+                registroAtual--;
+                navegar();
+            }
+        }
+
+        private void btnUltimo_Click(object sender, EventArgs e)
+        {
+             if(registroAtual<totalregistros-1)
+            {
+                registroAtual = totalregistros - 1;
+                navegar();
+            }
+        }
+
+        private void btnPrimeiro_Click(object sender, EventArgs e)
+        {
+            if (registroAtual > 0)
+            {
+                registroAtual = 0;
+                navegar();
+            }
         }
     }
 }
